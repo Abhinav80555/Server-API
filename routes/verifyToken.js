8c5ec400439156bb5fbaefef5f3b071d8a93dbb3
abhinav <99832265+Abhinav80555@users.noreply.github.com>
2022-06-25 05:27:55 +0000

    bafore finally api

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -19,17 +19,17 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      res.status(403).json("You are not allowed to do that!");
+      res.status(403).json("You are not alowed to do that!");
     }
   });
 };
 
 const verifyTokenAndAdmin = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.isAdmin) {m
+    if (req.user.isAdmin) {
       next();
     } else {
-      res.status(403).json("You are not allowed to do that!");
+      res.status(403).json("You are not alowed to do that!");
     }
   });
 };
